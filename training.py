482a2d5fd945ccd2d91e850a2a834e5cca6a8d1f
kPereirathefirst <kaelyn.a.pereira@gmail.com>
2024-10-29 19:18:21 -0400

    changed training and cam

--- training.py
+++ training.py
@@ -1,6 +1,20 @@
 import tensorflow as tf
 import keras 
+from keras.datasets import mnist
+import numpy as np
+from matplotlib import pyplot as plt 
 
-keras.datasets.minst.load_data(path="mnist.npz")
+#load dataset
+(train_X, train_y), (test_X, test_y) = mnist.load_data()
 
+#print shapes of vectors? 
+print('X_train: ' + str(train_X.shape))
+print('Y_train: ' + str(train_y.shape))
+print('X_test:  '  + str(test_X.shape))
+print('Y_test:  '  + str(test_y.shape))
 
+#plot the dataset 
+for i in range(9):  
+    plt.subplot(330 + 1 + i)
+    plt.imshow(train_X[i], cmap=plt.get_cmap('gray'))
+    plt.show()
